538ab312482d17981ac81fada6b74a44a8ba587c
suzanne0401 <marciniakzuzanna147@gmail.com>
2025-11-06 11:47:41 +0100

    komentarz

---
+++ pierwsza_gra.py
@@ -0,0 +1,20 @@
+import random
+
+def roll_dice():
+    return random.randint(1, 6)
+
+def play_game():
+    print("Welcome to the Dice Rolling Game!")
+
+    while True:
+        input("Press Enter to roll the dice...")
+        roll = roll_dice()
+        print(f"You rolled a {roll}!")
+
+        play_again = input("Roll again? (y/n): ").strip().lower()
+        if play_again != 'y':
+            print("Thanks for playing!")
+            break
+
+print("Hello World")
+play_game()
